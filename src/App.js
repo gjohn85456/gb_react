e36d8285417dbdbf07cd4ff897002dfa3130d788
Evgeniy <gjohn85456@gmail.com>
2022-01-15 19:55:45 +0500

    refactor: task1

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function Message(props) {
         className={`App-header ${props.showRed ? 'header-red' : 'header-blue'}`}
         style={{ top: props.topPosition || '10px' }}
       >
-        My first React App
+        Finished first task!
         <h3>Hello, {props.name}</h3>
       </header>
     </div>
