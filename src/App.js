1c3e9ac0c3bdf5932f781d7b3f16c18d2f8ea994
Evgeniy <gjohn85456@gmail.com>
2022-01-29 16:18:11 +0500

    init: start task3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import { useEffect } from 'react';
 import { useState } from 'react/cjs/react.development';
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
+import UnstyledInput from './mu_input';
+
 import './App.scss';
 
 function App(props) {
   const [messages, setMessage] = useState([]);
   const [input, setInput] = useState({});
+
   useEffect(() => {
-    setTimeout(function () {
+    let timer;
+    timer = setInterval(function () {
       if (messages.length != 0) {
         if (messages[messages.length - 1].author === "human") {
           let arr = [];
@@ -16,35 +22,56 @@ function App(props) {
         }
       }
     }, 1500);
+    return () => {
+      clearInterval(timer);
+    }
   }, [messages])
 
 
   const handleBtn = () => {
-    setMessage((preMessage) => {
-      /*
-      почему это не работает??? 
-      messages/preMessage это массив почему мы не можем использовать методы массива для его изменения?
-      input.id = preMessage.length;
-      return preMessage.push(input)
-      */
-      let arr = [];
-      input.id = preMessage.length;
-      arr = preMessage.slice();
-      let test = { ...input };
-      arr.push(test);
-      return preMessage = arr.slice();
-    })
+    console.log(input);
+    input.id = messages.length;
+    console.log(input.text);
+    // setMessage(...messages, { text: input.text, author: input.author, id: input.id });
+    setMessage([...messages, { text: input.text, author: input.author, id: input.id }]);
+    console.log(messages);
+    // setMessage((preMessage) => {
+    //   let arr = [];
+    //   input.id = preMessage.length;
+    //   arr = preMessage.slice();
+    //   let test = { ...input };
+    //   arr.push(test);
+    //   return preMessage = arr.slice();
+    // })
   }
-
+  const f = (e) => {
+    setInput({ text: e.target.value, author: "human" })
+  }
 
   return (
     <div className="App">
-      <input type="text" placeholder="your text" onChange={e => setInput({ text: e.target.value, author: "human" })} />
-      <button onClick={handleBtn}>Btn</button>
-      {messages.map(message => {
-        return <div key={message.id}>{message.text} - say: {message.author}</div>
-      })}
-    </div>
+      <div className='container'>
+        <div className='messages_space'>
+          {messages.map((message, id) => (
+            <div className={`messages_${message.author === 'human' ? 'human' : 'robot'}`} key={message.id}>
+              {message.text} - say: {message.author}
+            </div>
+          ))}
+        </div>
+        <div className='messages_toolbar'>
+          <div>
+            <UnstyledInput myEvent={f}></UnstyledInput>
+          </div>
+
+          {/* <input type="text" placeholder="your text" onChange={e => setInput({ text: e.target.value, author: "human" })} /> */}
+          <div>
+            <Button variant="contained" endIcon={<SendIcon />} onClick={handleBtn}>Send</Button>
+          </div>
+        </div>
+
+      </div>
+
+    </div >
 
   );
 }
